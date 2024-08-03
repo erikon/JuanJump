c8516238ed50f40a3b37b4b89650ba05f37a556e
Eric Kong <eric.kong98@gmail.com>
2024-08-03 15:49:09 -0400

    temp

--- scripts/player.gd
+++ scripts/player.gd
@@ -17,7 +17,6 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		$AnimatedSprite2D.animation = "jump"
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction: -1, 0, 1
@@ -28,6 +27,12 @@ func _physics_process(delta):
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
+	
+# Play animations
+	if is_on_floor():
+		animated_sprite.play("idle")
+	else:
+		animated_sprite.play("jump")
 	
 	# Apply movement to character
 	if direction:
