d19a917a13e37c904da6b6195ff51a9133039f90
Eric Kong <eric.kong98@gmail.com>
2024-08-03 18:38:34 -0400

    Randomly Spawning Platforms

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,14 +2,14 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
+const JUMP_VELOCITY = -800.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
 
-func _physics_process(delta):
+func _physics_process(delta) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
