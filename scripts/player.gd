3779d894718a95eafc5b5cbda25c61941715e037
hakjoonlee <hakjl68@gmail.com>
2024-08-03 15:42:43 -0400

    Add jump animation
    Add jump animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -17,6 +17,7 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		$AnimatedSprite2D.animation = "jump"
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction: -1, 0, 1
